743fdf3d8e1aca5195806d91724c63e3d82564b1
Himanshu303 <himanshumarathe1611@gmail.com>
2022-07-12 17:37:33 +0530

    Added Add method

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -4,10 +4,15 @@ namespace HelloWorldApp
 {
     class Program
     {
+        static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("My name is Himanshu");
+            Add(5, 2);
         }
     }
 }
